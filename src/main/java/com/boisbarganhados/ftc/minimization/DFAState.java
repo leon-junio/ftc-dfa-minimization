0fd3156e9356e73cd5fdfa83256e717ec4093079
Leon Junio Martins Ferreira <juniohuy2gamer@gmail.com>
2024-04-27 00:42:59 -0300

    pre process initial and final boolean tags

--- src/main/java/com/boisbarganhados/ftc/minimization/DFAState.java
+++ src/main/java/com/boisbarganhados/ftc/minimization/DFAState.java
@@ -2,17 +2,21 @@ package com.boisbarganhados.ftc.minimization;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 
 import com.boisbarganhados.ftc.jflap.utils.State;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 public class DFAState {
     private int id;
+    private List<Integer> ids;  
     private String name;
     private boolean initialState, finalState;
+    @EqualsAndHashCode.Exclude
     private Map<String, HashSet<DFAState>> transitions;
 
     public DFAState(int i) {
@@ -38,5 +42,16 @@ public class DFAState {
         state.setStateInitial(this.isInitialState());
         state.setStateFinal(this.isFinalState());
         return state;
+    }
+
+    @Override
+    public String toString() {
+        return "DFAState{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", initialState=" + initialState +
+                ", finalState=" + finalState +
+                ", transitions=" + transitions.keySet() +
+                '}';
     }
 }
